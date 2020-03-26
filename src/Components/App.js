d4d37f2020c69ecf002dffba592328a3cb9976d6
shivam0anand <shivam_anand@outlook.com>
2020-03-26 13:32:59 +0530

    style

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./header";
 
 function App() {
-  return <h1>hiii</h1>;
+  return <Header />;
 }
 
 export default App;
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "../stylesheet/reset.css";
+import "../stylesheet/style.css";
 
-class Header extends React.Component {}
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="wrapper">
+        <h3>COVID-19</h3>
+      </nav>
+    );
+  }
+}
 
 export default Header;
