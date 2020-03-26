add89e772ed8ae7eb8622ba11f0bdd099a636570
shivam0anand <shivam_anand@outlook.com>
2020-03-26 23:10:22 +0530

    add component

---
+++ src/Components/graph.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CanvasJSReact from "../canvasjs-2.3.2/canvasjs.react";
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+class Graph extends React.Component {}
