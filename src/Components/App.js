777931ed67fa4796392d1ee71cc137b9d3819635
shivam0anand <shivam_anand@outlook.com>
2020-03-28 20:58:27 +0530

    modified

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "./header";
 import Cases from "./cases";
 import World from "./world";
-// import Graph from "./graph";
 
 function App() {
   return (
     <>
       <Header />
       <World />
-      {/* <Graph /> */}
       <Cases />
       <div>
         <h4 className="nametag">Made by Shivam Anand</h4>
