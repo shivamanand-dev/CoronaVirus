9ad30a1e504fbe48cc78c12e3d8f3ef5011fbd6e
shivam0anand <shivam_anand@outlook.com>
2020-03-28 20:57:59 +0530

    modified

---
+++ src/Components/mortality.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+const data = {
+  labels: ["Death", "Active Cases"],
+  datasets: [
+    {
+      data: [],
+      backgroundColor: ["#FF6384", "#36A2EB"]
+      //   hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+    }
+  ]
+};
+
+const doughnut = cases => {
+  let active = cases.cases.cases - cases.cases.deaths - cases.cases.recovered;
+  data.datasets[0].data.push(cases.cases.recovered + active);
+  data.datasets[0].data.push(cases.cases.deaths);
+
+  data.datasets[0].data = data.datasets[0].data.filter(Number);
+
+  //   let mortality =
+
+  return (
+    <>
+      <Doughnut data={data} />
+      {console.log(cases)}
+      {console.log(data.datasets[0].data)}
+      {console.log(active)}
+    </>
+  );
+};
+
+export default doughnut;
