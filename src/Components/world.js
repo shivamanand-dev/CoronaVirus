7083443cefc9805ffaec6a5fbc7fa9546feeea57
shivam0anand <shivam_anand@outlook.com>
2020-03-28 16:50:51 +0530

    improved styles

--- src/Components/world.js
+++ src/Components/world.js
@@ -16,23 +16,30 @@ class World extends React.Component {
   render() {
     return (
       <>
-        <section className="wrapper">
-          <h2 className="world">World</h2>
-          <h2 className="caption">
-            A Real time report of all the{" "}
-            <span className="covid">COVID-19</span> patient around the globe
-          </h2>
-          {console.log(this.state.worldReport)}
-          <div className="table grid-col-2">
-            <div className="title">
-              <h4>Total cases:</h4>
-              <h4>Total Deaths:</h4>
-              <h4>Total Recovered:</h4>
-            </div>
-            <div className="numbers">
-              <h4>{this.state.worldReport.cases}</h4>
-              <h4>{this.state.worldReport.deaths}</h4>
-              <h4>{this.state.worldReport.recovered}</h4>
+        <section className="world">
+          <div className="wrapper">
+            <h2 className="world-text">World</h2>
+
+            <h2 className="caption">
+              A Real time report of all the{" "}
+              <span className="covid">COVID-19</span> patient around the globe
+            </h2>
+            {console.log(this.state.worldReport)}
+            <div className="grid-col-3">
+              <div className="table grid-col-2">
+                <div className="title">
+                  <h4>Total cases:</h4>
+                  <h4>Total Deaths:</h4>
+                  <h4>Total Recovered:</h4>
+                </div>
+                <div className="numbers">
+                  <h4>{this.state.worldReport.cases}</h4>
+                  <h4>{this.state.worldReport.deaths}</h4>
+                  <h4>{this.state.worldReport.recovered}</h4>
+                </div>
+              </div>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </section>
