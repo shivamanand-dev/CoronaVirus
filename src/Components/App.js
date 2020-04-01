f20aa96d08efd63b98a077cc90336ffc366372d8
shivam0anand <shivam_anand@outlook.com>
2020-04-01 17:37:57 +0530

    add death.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./header";
 import Cases from "./cases";
 import World from "./world";
+import Death from "./death";
 
 function App() {
   return (
     <>
       <Header />
       <World />
+      <Death />
       <Cases />
       <div>
         <h4 className="nametag">Made by Shivam Anand</h4>
