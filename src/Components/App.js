b01038f13399a78837feb9f9772345e591b4350d
shivam0anand <shivam_anand@outlook.com>
2020-04-02 18:06:41 +0530

    bug fix

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "./header";
 import Cases from "./cases";
 import World from "./world";
-import Death from "./death";
 
 function App() {
   return (
     <>
       <Header />
       <World />
-      <Death />
       <Cases />
       <div>
         <h4 className="nametag">Made by Shivam Anand</h4>
